cf0667d1d30742f385fa91f0eaae48aada9e26ce
Sunny Heyar <sunny.heyar@gmail.com>
2022-04-23 20:35:40 -0400

    removed console.logs and general cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import SearchBar from "./components/SearchBar";
 import Weather from "./components/Weather";
 import {
   Chart as ChartJS,
@@ -56,29 +55,6 @@ const App = () => {
   //     });
   //   });
   // };
-
-  // const searchComponent = (
-  //   <div className="h-screen flex flex-col">
-  //     <SearchBar setLocation={setLocation} setLoading={setLoading} />
-
-  //     <div className="text-white text-center w-screen md:max-w-[768px] md:mx-auto">
-  //       <div className="underline mt-4 underline-offset-4">
-  //         Favorite Locations
-  //       </div>
-  //       {favoriteLocations.map(favoriteLocation => (
-  //         <div
-  //           key={favoriteLocations.indexOf(favoriteLocation)}
-  //           className="cursor-pointer p-2 my-2 mx-4 bg-black/30 rounded-lg"
-  //           onClick={() => {
-  //             setLocation(favoriteLocation);
-  //           }}
-  //         >
-  //           {`${favoriteLocation.name}, ${favoriteLocation.state}, ${favoriteLocation.country}`}
-  //         </div>
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
 
   const weatherComponent = (
     <>
